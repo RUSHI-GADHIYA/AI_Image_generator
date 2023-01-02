33756ff55bf7e516ae3054aadf0e49ebd48614f4
rushi gadhiya <rushi.gadhiya@gmail.com>
2023-01-01 19:17:21 -0600

    added frontend

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-
+const path = require("path")
 // body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/openai', require('./routes/openaiRoutes'))
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.listen(port, () => { console.log("server started on", port) })
